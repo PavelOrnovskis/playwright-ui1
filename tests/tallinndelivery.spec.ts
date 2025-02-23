0999f4d0d579fba91efbacf050de03c0edc00883
pavel <pavel.ornovskis@gmail.com>
2025-02-24 00:19:19 +0200

    Initial commit

---
+++ tests/tallinndelivery.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from "@playwright/test";
+import {faker} from "@faker-js/faker/locale/en";
+
+
+
+test.describe("Negative Login testing on Tallinn Delivery website", async () => {
+
+
+} )
